468c4b0c575e72218fb071847bd775890411cb9f
Eric Patrick <erstpa@gmail.com>
2018-07-09 16:21:12 -0400

    Moved browser-sync to port 5000

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -100,7 +100,8 @@ function inline() {
 // Start a server with LiveReload to preview the site in
 function server(done) {
   browser.init({
-    server: 'dist'
+    server: 'dist',
+    port: 5000
   });
   done();
 }
